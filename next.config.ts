36044e03b39c332ee9961cf6e80c25190ce6020b
AjwadDaiki <148894088+AjwadDaiki@users.noreply.github.com>
2026-01-24 17:30:37 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,7 @@ import type { NextConfig } from "next";
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   experimental: {
-    // Optimisation server actions
     serverActions: {
       bodySizeLimit: "10mb",
     },
